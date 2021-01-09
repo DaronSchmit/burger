a94500aea539d0f65874c9630c7ff7cce8c0b52b
Daron Schmit <darons23@gmail.com>
2021-01-09 15:45:00 -0600

    added jaws compatibility for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 // MySQL Setup
-var mysql = require("mysql");
+const mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "vegetable",
-  database: "burger_db"
-});
+let connection;
+
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "vegetable",
+    database: "burger_db"
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
